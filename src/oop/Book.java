f22a71d30ee9533a1e8551c1f7a1bfcb6cb4b3a8
Mark Mwima <markmwimar@gmail.com>
2024-07-19 13:00:27 +0300

    book getters and constructor

--- src/oop/Book.java
+++ src/oop/Book.java
@@ -1,10 +1,24 @@
 package oop;
 
 public class Book {
-    public String title;
-    public String author;
+    private String title;
+    private String author;
+
+    Book(String title, String author){
+        this.title=title;
+        this.author=author;
+    }
+
+    String getTitle(){
+        return this.title;
+            }
+            String getAuthor(){
+        return this.author;
+            }
 
     public String toString(){
         return String.format("%s by %s", this.title,this.author);
     }
+
+
 }
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -5,15 +5,15 @@ import java.time.LocalDate;
     public static void main(String[] args) {
 
 
-        User user = new User("Yungeen Ace","1993-05-15");
-        Book book = new Book();
+        User user = new User("Yungeen Ace","1993-05-15");//Abstraction is hiding complexities of a class
+        Book book = new Book("Absolute Power","Robert Anguka");
 //        user.name = "Yungeen Ace";
 //        user.birthDay=LocalDate.parse("1993-05-15");
         user.borrow(book);
         System.out.printf("%s was born in %s, and is %d years old.\n",user.getName(), user.age(), user.age());
-        book.title="Absolute Power";
-        book.author="Robert Anguka";
-        System.out.printf("%s was authored by %s\n", book.title,book.author);
+//        book.title="Absolute Power";
+//        book.author="Robert Anguka";
+        System.out.printf("%s was authored by %s\n", book.getTitle(),book.getAuthor());
         System.out.printf("%s has borrowed %s\n",user.getName(), user.books.toString());
     }
 }
