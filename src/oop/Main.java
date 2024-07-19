d395aecf0874f6e83319a497b2920653351b017e
Mark Mwima <markmwimar@gmail.com>
2024-07-19 08:09:07 +0300

    additional user objects

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -7,5 +7,10 @@ User youngerUser = new User();
 youngerUser.name="Kodak Black";
 youngerUser.birthDay= LocalDate.parse("1998-04-14");
         System.out.printf("%s was born in %s, and is %d years old.",youngerUser.name,youngerUser.birthDay.toString(), youngerUser.age());
+
+        User olderUser = new User();
+        olderUser.name = "Yungeen Ace";
+        olderUser.birthDay=LocalDate.parse("1993-05-15");
+        System.out.printf("%s was born in %s, and is %d years old.",olderUser.name,olderUser.birthDay.toString(), olderUser.age());
     }
 }
