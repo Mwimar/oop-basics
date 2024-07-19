b6c757db66b5fe77f62b35384559e52cfeb984af
Mark Mwima <markmwimar@gmail.com>
2024-07-19 11:56:06 +0300

    Book class

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -6,8 +6,12 @@ import java.time.LocalDate;
 
 
         User User = new User();
+        Book book = new Book();
         User.name = "Yungeen Ace";
         User.birthDay=LocalDate.parse("1993-05-15");
-        System.out.printf("%s was born in %s, and is %d years old.",User.name,User.birthDay.toString(), User.age());
+//        System.out.printf("%s was born in %s, and is %d years old.",User.name,User.birthDay.toString(), User.age());
+        book.title="Absolute Power";
+        book.author="Robert Anguka";
+        System.out.printf("%s was authored by %s", book.title,book.author);
     }
 }
