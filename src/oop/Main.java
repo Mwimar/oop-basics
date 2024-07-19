c5a00fa3a001a240107ea3868d49c23d0132f3a9
Mark Mwima <markmwimar@gmail.com>
2024-07-19 08:01:28 +0300

    age method

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -6,6 +6,6 @@ import java.time.LocalDate;
 User youngerUser = new User();
 youngerUser.name="Kodak Black";
 youngerUser.birthDay= LocalDate.parse("1998-04-14");
-        System.out.printf("%s was born in %s",youngerUser.name,youngerUser.birthDay);
+        System.out.printf("%s was born in %s, and is %d years old.",youngerUser.name,youngerUser.birthDay.toString(), youngerUser.age());
     }
 }
