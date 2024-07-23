c9a3292d3f7f385ecf4cb2cc051d9f313cdeae1c
Mark Mwima <markmwimar@gmail.com>
2024-07-23 11:03:05 +0300

    inheriting properties

--- src/oop/AudioBook.java
+++ src/oop/AudioBook.java
@@ -1,4 +1,8 @@
 package oop;
 public class AudioBook extends Book {
-
+private int runTime;
+AudioBook(String title, String author, Integer pageCount, Integer runTime){
+    super(title,author,pageCount);
+    this.runTime=runTime;
+}
 }
